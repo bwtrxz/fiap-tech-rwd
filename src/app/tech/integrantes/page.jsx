774142044fe55c542a9a52824fb674b506fab162
ruanJS <rm551096@fiap.com.br>
2023-11-10 17:10:00 -0300

    Add integrantes page component

---
+++ src/app/tech/integrantes/page.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function Integrantes() {
+  return (
+    <>
+        <div>
+            <h1>Integrantes</h1>
+                <p>
+                    Ana Beatriz -
+                    RM552536
+                </p>
+                <p>
+                    Bruno da Silva Albino Antunes -
+                    RM98470
+                </p>
+                <p>
+                    Douglas Cavichioli -
+                    RM551100
+                </p>
+                <p>
+                    Pedro Henrique Ferrari -
+                    RM550231
+                </p>
+                <p>
+                    Ruan Guedes de Campos -
+                    RM551096
+                </p>
+                <p>
+                    Rafael Lino -
+                    RM551577
+                </p>
+        </div>
+    </>
+  )
+}
