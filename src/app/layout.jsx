25e932f56f918482c25f61da5730fd6e5c37ba2a
CavichioliFIAP <rm551100@fiap.com.br>
2023-11-14 15:37:24 -0300

    modificando conteudo

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt">
       <body className={inter.className}>
       <Cabecalho/>
         {children}
