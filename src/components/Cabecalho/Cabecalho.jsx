ae82ef9f961bb94c1e6b0fca09b70abe05ee991e
CavichioliFIAP <rm551100@fiap.com.br>
2023-11-14 14:37:54 -0300

    adicionando icon no header

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,25 +1,31 @@
 import Link from "next/link";
+import Image from "next/image";
 import "./Cabecalho.scss";
 
-export default function Cabecalho(){
-    return(
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href= "/">Página Inicial</Link>
-                    </li>
-                    <li>
-                        <Link href= "/tech/desafios">Desafios</Link>
-                    </li>
-                    <li>
-                        <Link href= "/tech/solucoes">Soluções</Link>
-                    </li>
-                    <li>
-                        <Link href= "/tech/integrantes">Integrantes</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+export default function Cabecalho() {
+  return (
+    <header>
+      <nav>
+        <div className="logo">
+            <a href="/">
+                <Image src="/img/logo-porto-seguro.png" alt="image" width={200} height={300} />
+            </a>
+        </div>
+        <ul>
+          <li>
+            <Link href="/">Página Inicial</Link>
+          </li>
+          <li>
+            <Link href="/tech/desafios">Desafios</Link>
+          </li>
+          <li>
+            <Link href="/tech/solucoes">Soluções</Link>
+          </li>
+          <li>
+            <Link href="/tech/integrantes">Integrantes</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
