9dbf9c37d96a4c158e3bb8987b8137bd8e1c3b3a
CavichioliFIAP <rm551100@fiap.com.br>
2023-11-14 15:37:37 -0300

    alterando informações

--- src/app/tech/integrantes/page.jsx
+++ src/app/tech/integrantes/page.jsx
@@ -4,28 +4,30 @@ export default function Integrantes() {
   return (
     <>
         <div>
-            <h1>Integrantes</h1>
-                <p>
+            <h1 className='Integrantes'>
+                Integrantes
+            </h1>
+                <p className='Integrantes'>
                     Ana Beatriz -
                     RM552536
                 </p>
-                <p>
+                <p className='Integrantes'>
                     Bruno da Silva Albino Antunes -
                     RM98470
                 </p>
-                <p>
+                <p className='Integrantes'>
                     Douglas Cavichioli -
                     RM551100
                 </p>
-                <p>
+                <p className='Integrantes'>
                     Pedro Henrique Ferrari -
                     RM550231
                 </p>
-                <p>
+                <p className='Integrantes'>
                     Ruan Guedes de Campos -
                     RM551096
                 </p>
-                <p>
+                <p className='Integrantes'>
                     Rafael Lino -
                     RM551577
                 </p>
